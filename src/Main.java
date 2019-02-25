4d3d3eeda095740a092936407d2430f7b5d9e157
daminor26 <daminor26@gmail.com>
2019-02-25 15:40:09 +0100

    connexion completed

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
 import controller.*;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import mocks.FoodMocks;
 import mocks.UserMocks;
-import model.User;
-import view.ViewInscription;
 import javafx.stage.Stage;
+import model.User;
 import view.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage){
-        ViewBase view = new ViewGestionMenu();
-        Controller controller = new ControllerGestionMenu(primaryStage, null, view);
-        UserMocks.initMocks();
         FoodMocks.initMocks();
+        UserMocks.initMocks();
+        User.actualUser = UserMocks.users.get("todesco@gmail.com");
+
+        ViewBase view = new ViewAccueil();
+        Controller controller = new ControllerAccueil(primaryStage, null, view);
+        controller.setView(controller);
     }
 
     public static void main(String[] args) {
--- src/controller/ControllerLancement.java
+++ src/controller/ControllerLancement.java
@@ -17,30 +17,8 @@ public class ControllerLancement extends Controller {
         super(stage,previousController, actualView);
     }
 
-
     public void init(){
         connexionButton.setOnAction( event -> setView(new ControllerConnexion(getStage(), this, new ViewConnexion())));
         inscriptionButton.setOnAction( event -> setView(new ControllerInscription(getStage(), this, new ViewInscription())));
     }
-
-    /*
-    public void goToConnexionView() {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            ControllerConnexion controller = new ControllerConnexion();
-            loader.setController(controller);
-            Parent root = loader.load(getClass().getResourceAsStream("../"+ViewConnexion.XML_FILE));
-            root.getStylesheets().add(ViewConnexion.CSS);
-            controller.init();
-
-            Stage stage = (Stage) connexionButton.getScene().getWindow();
-            stage.setScene(new Scene(root, ViewConnexion.WIDTH, ViewConnexion.HEIGHT));
-            stage.setTitle(ViewConnexion.LABEL);
-            stage.show();
-
-        }catch (IOException io){
-            io.printStackTrace();
-        }
-    }
-    */
 }
