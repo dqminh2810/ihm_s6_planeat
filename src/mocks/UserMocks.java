bd251abdc3ea01ae540e1ff6083b89ff09beba19
daminor26 <daminor26@gmail.com>
2019-02-25 15:17:33 +0100

    mocks with dish

--- src/mocks/UserMocks.java
+++ src/mocks/UserMocks.java
@@ -1,11 +1,10 @@
 package mocks;
 
-import model.CookingFrequency;
-import model.StatusChoice;
-import model.User;
-import model.UserSex;
+import model.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class UserMocks {
@@ -14,10 +13,20 @@ public class UserMocks {
     public static void initMocks(){
         users = new HashMap<>();
         User user;
+        Repas repas;
+        ArrayList<Dish> dish = new ArrayList<>();
+        ArrayList<Ingredient> ingredients = new ArrayList<>();
 
         user = new User("Todesco", "Gabin", LocalDate.now(),
                 "todesco@gmail.com", UserSex.MALE, 76, 185, StatusChoice.STUDENT, CookingFrequency.OFTEN, "test");
         users.put("todesco@gmail.com", user);
+        ingredients.add(new Ingredient(FoodMocks.foods.get(0), 6));
+        dish.add(new Dish("Omelette des familles", "A ne pas retourner", CourseType.MAIN_COURSE, ingredients, null));
+        repas = new Repas(dish, LocalDateTime.now());
+        user.addRepas(repas);
+
+        repas = new Repas(dish, LocalDateTime.of(2019, 2, 25, 12, 30));
+        user.addRepas(repas);
 
         user = new User("Bauce", "Camille", LocalDate.now(),
                 "bauce@gmail.com", UserSex.FEMALE, 49, 160, StatusChoice.STUDENT, CookingFrequency.NEVER, "azerty");
