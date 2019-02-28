82b58ea6fc2c80a163f29c033f413860524cb858
BauceCamille <camille.bauce@etu.unice.fr>
2019-02-28 17:41:04 +0100

    add FXML variable to accueil and set curDate text

--- src/controller/ControllerAccueil.java
+++ src/controller/ControllerAccueil.java
@@ -1,11 +1,17 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import view.ViewAddFood;
+import view.ViewAgenda;
 import view.ViewBase;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 public class ControllerAccueil extends Controller {
 
@@ -21,6 +27,30 @@ public class ControllerAccueil extends Controller {
     private Text curmealHourText;
     @FXML
     private Text curmealNameText;
+    @FXML
+    private Button statsButton;
+    @FXML
+    private PieChart caloriesPiechart;
+    @FXML
+    private PieChart proteinPiechart;
+    @FXML
+    private PieChart glucosesPiechart;
+    @FXML
+    private PieChart dontsucrePiechart;
+    @FXML
+    private Button agendaButton;
+    @FXML
+    private Button addMealButton;
+    @FXML
+    private Button createMealButton;
+    @FXML
+    private Button createIngredientButton;
+    @FXML
+    private Button profileButton;
+    @FXML
+    private Text curHourText;
+
+
 
 
     public ControllerAccueil(Stage stage, Controller previousController, ViewBase previousView) {
@@ -28,7 +58,16 @@ public class ControllerAccueil extends Controller {
     }
 
     public void init(){
+        //date du jour
+        LocalDate day = LocalDate.now();
+        curdateText.setText(day.toString());
 
+        //association button
+        agendaButton.setOnAction(event -> setView(new ControllerAgenda(getStage(), this, new ViewAgenda())));
+        createIngredientButton.setOnAction(event -> setView(new ControllerAddFood(getStage(), this, new ViewAddFood())));
+        //createMealButton.setOnAction(event ->  new Con);
+        //addMealButton.setOnAction(event -> setView(new ControllerAddDish(getStage(), this, new ViewAddDish())));
+        //profileButton.setOnAction(event -> setView(new ControllerProfile(getStage(), this, new ViewProfile())));
     }
 
 }
